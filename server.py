618f7b3b8f840ceb223686d15de000be1ef1100e
Nikolett Lakos <lakosnikolett@gmail.com>
2018-01-07 00:48:01 +0100

    End of the day push.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import data_manager
 import time
+import common
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,19 +14,24 @@ def asking_question():
     if request.method == 'GET':
         return render_template('ask_a_question_form.html')
     elif request.method == 'POST':
-        data_manager.write_in_file('answer.csv')
-        return render_template('/')
+        question = data_manager.read_file("question.csv")
+        form = request.form.to_dict()
+        form_list = [common.id_generator(question), form['question_title'], form['question_detail'], form['category'], time.time()]
+        question.append(form_list)
+        data_manager.write_in_file("question.csv", question)
+        return redirect("/")
+
 
 @app.route('/write_answer', methods=['POST', 'GET'])
 def writing_answer():
     return render_template("write_an_answer_form.html")
 
-'''
+
 @app.route('/question/<id>', methods=['GET'])
-def question_details():
-    id = data_manager.read_file('answer.csv'[])
-    return render_template("question_details.html", id=id)
-'''
+def question_details(id):
+    lines = data_manager.read_file('question.csv')
+    return render_template("question_details.html", id=id, lines=lines)
+
 
 if __name__ == "__main__":
     app.run(
