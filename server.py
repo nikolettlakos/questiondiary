f1579d0d821b14a6a4fef73c54644d855bf13e5f
Nikolett Lakos <lakosnikolett@gmail.com>
2018-01-08 10:46:14 +0100

    Start of the day push because why not.

--- server.py
+++ server.py
@@ -20,13 +20,23 @@ def asking_question():
 
         lines.append(data_list)
 
-        data_manager.write_in_file("question.csv", lines)
-        return redirect("/")
+        data_manager.write_in_file('question.csv', lines)
+        return redirect('/')
 
 
 @app.route('/write_answer', methods=['POST', 'GET'])
 def writing_answer():
-    return render_template("write_an_answer_form.html")
+    if request.method == 'GET':
+        return render_template("write_an_answer_form.html")
+    elif request.method == 'POST':
+        lines = data_manager.read_file('answer.csv')
+        data_dict = request.form.to_dict()
+        data_list = [data_dict['answer'], time.time()]
+
+        lines.append(data_list)
+
+        data_manager.write_in_file('answer.csv', lines)
+        return redirect('/')
 
 
 @app.route('/question/<id>', methods=['GET'])
@@ -34,6 +44,19 @@ def question_details(id):
     lines = data_manager.read_file('question.csv')
     data = common.details_by_id('question.csv', id)
     return render_template("question_details.html", id=id, lines=data)
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+    lines = data_manager.read_file('question.csv')
+    if request.method == 'GET':
+        render_template('edit_form.html', id=id, lines=lines)
+    elif request.method == 'POST':
+        data_dict = request.form.to_dict()
+        lines[1] = data_dict['question_title']
+        lines[2] = data_dict['question_detail']
+        lines[3] = data_dict['category']
+
+        return redirect('/')
 
 if __name__ == "__main__":
     app.run(
